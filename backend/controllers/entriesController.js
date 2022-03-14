3956125ebe45e12ca942e84ab4774062b218aefb
Sneha <snhthakare@gmail.com>
2022-03-14 18:16:41 +0530

    Adds meal creation functionality

--- backend/controllers/entriesController.js
+++ backend/controllers/entriesController.js
@@ -5,8 +5,13 @@ const getEntries = async (req, res) => {
     res.status(200).json(meal)
 }
 
-const setEntries = (req, res) => {
-    res.status(201).json({ message: 'Hello World!' })
+const setEntries = async (req, res) => {
+    const { meal, food } = req.body;
+    const newMeal = await Food.create({
+        meal, food
+    })
+
+    res.status(200).json(newMeal)
 }
 
 const updateEntries = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const { getEntries, setEntries, updateEntries, deleteEntries } = require('./controllers/entriesController.js')
 const connectDB = require('./config/db')
 const app = express()
 const port = 5000
-
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json({ extended: true }))
 connectDB()
 app.listen(port)
 
